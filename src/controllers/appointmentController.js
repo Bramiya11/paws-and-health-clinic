4efb518491069b7dec937bad82e70789ae54d220
Bramiya11 <brandon-ramirez-16@hotmail.com>
2026-03-21 22:43:52 -0500

    The MVC Decoupling task is performed and the Weight, Temperature and Diagnosis fields are created for Medical Consultations

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -1,20 +1,7 @@
-const db = require('../config/db.js');
+const AppointmentModel = require('../models/appointmentModel.js');
 
 exports.getAllAppointments = (req, res) => {
-    const sql = `
-        SELECT 
-            a.id,
-            o.owner_name,
-            p.pet_name,
-            a.service,
-            a.appointment_date,
-            a.status
-        FROM appointments a
-        JOIN pet p ON a.id_pet = p.id
-        JOIN owner o ON a.id_owner = o.id
-        ORDER BY a.appointment_date DESC
-    `;
-    db.all(sql, [], (err, rows) => {
+    AppointmentModel.getAllAppointments((err, rows) => {
         if (err) return res.status(500).send(err.message);
         res.render('index', { title: 'Panel de Citas', appointments: rows });
     });
@@ -25,69 +12,15 @@ exports.getCreateForm = (req, res) => {
 };
 
 exports.createAppointment = (req, res) => {
-    const { pet_name, owner_name, service, appointment_date } = req.body;
-
-    // 1. Insertar owner
-    db.run("INSERT INTO owner (owner_name) VALUES (?)", [owner_name], function (err) {
-        if (err) return res.status(500).send(err.message);
-        const ownerId = this.lastID;
-
-        // 2. Insertar pet con el id del owner
-        db.run("INSERT INTO pet (pet_name, id_owner) VALUES (?, ?)", [pet_name, ownerId], function (err) {
-            if (err) return res.status(500).send(err.message);
-            const petId = this.lastID;
-
-            // 3. Insertar appointment con ambos IDs
-            const sql = "INSERT INTO appointments (id_pet, id_owner, service, appointment_date) VALUES (?, ?, ?, ?)";
-            db.run(sql, [petId, ownerId, service, appointment_date], function (err) {
-                if (err) return res.status(500).send(err.message);
-                res.redirect('/');
-            });
-        });
-    });
-};
-
-exports.deleteAppointment = (req, res) => {
-    const id = req.params.id;
-    db.run("DELETE FROM appointments WHERE id = ?", id, function (err) {
+    AppointmentModel.createAppointment(req.body, (err) => {
         if (err) return res.status(500).send(err.message);
         res.redirect('/');
     });
 };
 
-/*exports.getAllAppointments = (req, res) => {
-    db.all("SELECT * FROM appointments ORDER BY appointment_date DESC", [], (err, rows) => {
-        if (err) {
-            return res.status(500).send(err.message);
-        }
-        res.render('index', { title: 'Panel de Citas', appointments: rows });
-    });
-};
-
-exports.getCreateForm = (req, res) => {
-    res.render('create', { title: 'Agendar Nueva Cita' });
-};
-
-exports.createAppointment = (req, res) => {
-    const { pet_name, owner_name, service, appointment_date } = req.body;
-    const sql = "INSERT INTO appointments (pet_name, owner_name, service, appointment_date) VALUES (?, ?, ?, ?)";
-    const params = [pet_name, owner_name, service, appointment_date];
-
-    db.run(sql, params, function (err) {
-        if (err) {
-            return res.status(500).send(err.message);
-        }
-        res.redirect('/');
-    });
-};
-
 exports.deleteAppointment = (req, res) => {
-    const id = req.params.id;
-    db.run("DELETE FROM appointments WHERE id = ?", id, function (err) {
-        if (err) {
-            return res.status(500).send(err.message);
-        }
+    AppointmentModel.deleteAppointment(req.params.id, (err) => {
+        if (err) return res.status(500).send(err.message);
         res.redirect('/');
     });
 };
-*/
---
+++ src/models/appointmentModel.js
@@ -0,0 +1,46 @@
+const db = require('../config/db.js');
+
+// Obtener todas las citas
+const getAllAppointments = (callback) => {
+    const sql = `
+        SELECT 
+            a.id,
+            o.owner_name,
+            p.pet_name,
+            a.service,
+            a.appointment_date,
+            a.status,
+            a.weight,
+            a.temperature
+        FROM appointments a
+        JOIN pet p ON a.id_pet = p.id
+        JOIN owner o ON a.id_owner = o.id
+        ORDER BY a.appointment_date DESC
+    `;
+    db.all(sql, [], callback);
+};
+
+// Crear una cita 
+const createAppointment = (data, callback) => {
+    const { pet_name, owner_name, service, appointment_date, weight, temperature, diagnosis } = data;
+
+    db.run("INSERT INTO owner (owner_name) VALUES (?)", [owner_name], function (err) {
+        if (err) return callback(err);
+        const ownerId = this.lastID;
+
+        db.run("INSERT INTO pet (pet_name, id_owner) VALUES (?, ?)", [pet_name, ownerId], function (err) {
+            if (err) return callback(err);
+            const petId = this.lastID;
+
+            const sql = "INSERT INTO appointments (id_pet, id_owner, service, appointment_date, weight, temperature, diagnosis) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            db.run(sql, [petId, ownerId, service, appointment_date, weight, temperature, diagnosis], callback);
+        });
+    });
+};
+
+// Eliminar una cita
+const deleteAppointment = (id, callback) => {
+    db.run("DELETE FROM appointments WHERE id = ?", [id], callback);
+};
+
+module.exports = { getAllAppointments, createAppointment, deleteAppointment };
